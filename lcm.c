6f8c6974f49953b2a55b5eb10559cb93d4744ca2
Rmmshv <32555704+Rmmshv@users.noreply.github.com>
2019-04-30 07:13:23 -0700

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lcm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: resheva <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/30 07:08:17 by resheva           #+#    #+#             */
+/*   Updated: 2019/04/30 07:11:43 by resheva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	lcm(unsigned int a, unsigned int b)
+{
+	int	gcd;
+	int	old_a;
+	int	old_b;
+
+	gcd = 0;
+	old_a = a;
+	old_b - b;
+	while (1)
+	{
+		if (a == 0)
+			break;
+		b %= a;
+		if (b == 0)
+			break;
+		a %= b;
+	}
+	gcd = (!b) ? a : b;
+	return (gcd ? (old_a / gcd * old_b) : 0);
+}
